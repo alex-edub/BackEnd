9e5ab9ae4abc909d9a15bfd0261cd66f9e59d3a2
Alex Baroncello <alexbaroncello@gmail.com>
2025-01-18 11:35:00 -0300

    o que eu fiz

--- database/database-connection.js
+++ database/database-connection.js
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize";
 
 const databaseConnection = new Sequelize("shop_dc", "root", "", {
   host: "localhost",
-  port: 3307,
+  port: 3,
   dialect: "mariadb",
   logging: false,
 });
--- index.js
+++ index.js
@@ -9,9 +9,18 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
+app.post("/", async (request, response) => {
+    const {name, email } = request.body; 
+    const users = await User.query(
+        "INSERT INTO users('name','birthdate')VALUES(?,?)"
+    );
+    return response.status(200).json('Dados salvos com sucesso');
+  });
+
 app.get("/", async (request, response) => {
+    const user = request.body;
   //const users = await User.sync();
-  return response.status(200).json("Minha app");
+  return response.status(200).json("Os dados a serem salvos são: &{user}");
 });
 
 const initApp = async () => {
